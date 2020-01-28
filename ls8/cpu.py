0c66568900f7ec2eb46aac944d7f2676d7f11549
Jason Prince <jasoncprince@gmail.com>
2020-01-27 20:48:39 -0700

    configured run()

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,6 +2,17 @@
 
 import sys
 
+# operation codes:
+HLT = 0b00000001
+LDI = 0b10000010
+PRN = 0b01000111
+MUL = 0b10100010
+ADD = 0b10100000
+PUSH = 0b01000101
+POP = 0b01000110
+CALL = 0b01010000
+RET = 0b00010001
+
 
 class CPU:
     """Main CPU class."""
@@ -17,6 +28,19 @@ class CPU:
         self.mar = 0
         self.mdr = 0
         self.fl = 0
+        self.isPaused = False
+
+        self.dispatch = {
+            HLT: self.op_HLT,
+            LDI: self.op_LDI,
+            PRN: self.op_PRN,
+            MUL: self.op_MUL,
+            PUSH: self.op_PUSH,
+            POP: self.op_POP,
+            CALL: self.op_CALL,
+            RET: self.op_RET,
+            ADD: self.op_ADD
+        }
 
     '''
     TODO: In CPU, add method ram_read() and ram_write() that access the RAM inside the CPU object.
@@ -82,4 +106,32 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+
+        '''
+        TODO: Step 3: Implement the core of CPU's run() method
+        It needs to read the memory address that's stored in register PC, and store that result in IR, the Instruction Register.
+        '''
+
+        while not self.isPaused:
+
+            # Using ram_read(), read the bytes at PC+1 and PC+2 from RAM into variables operand_a and operand_b in case the instruction needs them.
+            ir = self.ram[self.pc]
+            operand_a = self.ram_read(self.pc+1)
+            operand_b = self.ram_read(self.pc+2)
+
+            # Depending on the value of the opcode, perform the actions needed for the instruction per the LS-8 spec. Maybe an if-elif cascade...?
+            if ir in self.dispatch:
+                self.dispatch[ir](operand_a, operand_b)
+            else:
+                raise Exception(
+                    f'Unknown Instruction {bin(ir)} at {hex(self.pc)}')
+
+            # Check out the spec where it talks about instruction layout.
+            # The two high bits of the instruction tell you how many operands the instruction has. The value of those two bits plus one is the number of bytes you have to move the `PC`.
+            # Use `>>` and an `&` mask to extract those two bits, then add one to the result, then add that to the `PC` to get to the next instruction.
+
+            instruction_size = (ir >> 6) + 1
+            instruction_sets_pc = ((ir >> 4) & 0b1) == 1
+
+            if not instruction_sets_pc:
+                self.pc += instruction_size
